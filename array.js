b0c531f21fca1db527c0a349e3dfda01ccfab1c7
Javed-Patel-wohlig <javed.patel@wohlig.com>
2023-02-09 17:51:06 +0530

    after some changes

--- array.js
+++ array.js
@@ -1,10 +1,11 @@
 
 const array = [1,2,3,4,"one","two","three", 1.2,2.4,3.6,true,false]
 
-array.forEach(element => {
-    if (typeof(element)=== "string"){
-        console.log(element)
-    }else{
-        console.log(typeof(element))
+for(let i = 0; i < array.length; i++){
+    if(!(typeof array[i] === "string")){
+        array.splice(i, 1)
+        i--
     }
-})
+};
+
+console.log(array)
